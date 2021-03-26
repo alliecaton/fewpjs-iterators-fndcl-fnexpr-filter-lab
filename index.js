4fadcfe176e31f52c8afbec2dbf2fc61497097b1
Allie <alliecaton3@gmail.com>
2021-03-26 14:18:38 -0400

    commit tree 7d44ea5e4d0e8a0694b4035cfd07421c10b8d2d4

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching (drivers, string) {
+    let matchedDrivers = drivers.filter(driver => {
+        return driver.toLowerCase() === string.toLowerCase()
+    })
+    return matchedDrivers
+}
+
+function fuzzyMatch (drivers, string) {
+    let fuzzyMatched = drivers.filter(driver => {
+        return driver.startsWith(string)
+    })
+    return fuzzyMatched
+}
+
+function matchName(drivers, string) {
+    let matchedNames = drivers.filter(obj => {
+        return obj.name === string
+    })
+    return matchedNames
+}
